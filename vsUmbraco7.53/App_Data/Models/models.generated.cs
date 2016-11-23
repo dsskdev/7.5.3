9d6bf5cae5526541fc6b2d15e8cc2f802ec3c7d0
dssk dev <desskdev@gmail.com>
2016-11-23 16:21:56 +0000

    codebase update

--- vsUmbraco7.53/App_Data/Models/models.generated.cs
+++ vsUmbraco7.53/App_Data/Models/models.generated.cs
@@ -20,7 +20,7 @@ using Umbraco.ModelsBuilder.Umbraco;
 
 [assembly: PureLiveAssembly]
 [assembly:ModelsBuilderAssembly(PureLive = true, SourceHash = "3138019cbd2c5b01")]
-[assembly:System.Reflection.AssemblyVersion("0.0.0.3")]
+[assembly:System.Reflection.AssemblyVersion("0.0.0.2")]
 
 namespace Umbraco.Web.PublishedContentModels
 {
